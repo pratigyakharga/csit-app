f16dbd0f17243ced5e72866e6d5aa152218dc1be
PratigyaKharga <kchetrip@gmail.com>
2017-08-19 22:04:23 +0545

    Home.ts fixed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,11 +11,10 @@ export class HomePage {
 
   }
 
-<<<<<<< HEAD
+
   goToMainPage(){
-=======
-  onLoginClick(){
->>>>>>> b1a1bfc73e9f33b73066047855c5941cf3eb9168
+
+
   		this.navCtrl.push('SearchPage');
   }
 
