80f0f52110acaedc114ff4bc966ef3bc5e28520c
Prashant Ghimire <prashant.ghimire@leviton.com>
2017-11-28 10:01:46 -0600

    fixed module load error bug and added helper text on each tab pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { QuestionsPage } from '../pages/questions/questions';
-import { UnitTestPage } from '../pages/unit-test/unit-test';
-import { MidTermPage } from '../pages/mid-term/mid-term';
-import { PreBoardPage } from '../pages/pre-board/pre-board';
-import { FinalExamPage } from '../pages/final-exam/final-exam';
-import { UnitTestPageModule } from './unit-test.module';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {QuestionsPage} from '../pages/questions/questions';
 
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     QuestionsPage,
-    MidTermPage,
-    PreBoardPage,
-    FinalExamPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    UnitTestPageModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
     QuestionsPage,
-    UnitTestPage,
-    MidTermPage,
-    PreBoardPage,
-    FinalExamPage
   ],
   providers: [
     StatusBar,
@@ -45,4 +32,5 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
