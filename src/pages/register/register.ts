e1e14378149251eb6327450484f60e37a3c9fdd4
Pratigya Khadka <kchetrip@gmail.com>
2017-12-26 22:28:39 +0545

    Registration set up

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { Parse } from 'parse';
+import {HomePage} from "../home/home";
 
 @IonicPage()
 @Component({
   selector: 'page-register',
   templateUrl: 'register.html',
 })
+
 export class RegisterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  username : string;
+  email : string;
+  password : string;
+  password2 : string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl : AlertController) {
   }
 
   ionViewDidLoad() {
@@ -18,24 +25,43 @@ export class RegisterPage {
 
   registerUser(){
     console.log("Register Button Clicked");
-
+    if(this.password !== this.password2){
+      this.showPasswordMismatchError();
+      return;
+    }
 
     var user = new Parse.User();
-    user.set("username", "pratigya");
-    user.set("password", "admin123");
-    user.set("email", "kchetrip@gmail.com");
+    user.set("username", this.username);
+    user.set("password", this.password);
+    user.set("email", this.email);
     user.signUp(null, {
       success: (user) =>{
-        console.log(user);
+        console.log(user, 'success register');
+        this.navCtrl.push(HomePage);
       },
-      error: (user, error) => {
-       console.log(user,error);
+      error: (error) => {
+        this.showRegisterFailError(error);
       }
     });
-    
-
-
-
   }
 
+  showPasswordMismatchError() {
+    console.log('password not match!');
+    let alert = this.alertCtrl.create({
+      title: 'Password mismatch',
+      subTitle: 'Please re-enter the same password.',
+      buttons: ['Ok']
+    });
+    alert.present();
+  }
+
+    showRegisterFailError(error){
+    console.log(error, 'error register');
+      let alert = this.alertCtrl.create({
+        title: 'Registration failed',
+        subTitle: 'Your account couldn\'t be registered. Please provide different username or email.',
+        buttons: ['Ok']
+      });
+      alert.present();
+  }
 }
