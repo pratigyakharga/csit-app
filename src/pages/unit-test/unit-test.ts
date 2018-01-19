9ad61bc06e57fe6682b3043eb0d80ed12da7c86a
Pratigya Kharga <kchetrip@gmail.com>
2018-01-18 21:50:42 -0600

    removed unused dependencies

--- src/pages/unit-test/unit-test.ts
+++ src/pages/unit-test/unit-test.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component, NgZone} from '@angular/core';
+import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {Parse} from 'parse';
 
