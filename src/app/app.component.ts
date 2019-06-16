d2610b124a78f5ed3aae4cccfd2ab267af89018e
Pratigya Kharga <kchetrip@gmail.com>
2019-06-15 23:05:58 -0500

    route fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,17 +15,16 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any, action?: any}>;
+  pages: Array<{title: string, component: any, action?: any, backView?: boolean}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
       { title: 'Questions', component: QuestionsPage },
-      { title: 'Forum', component: 'ForumPage'},
-      { title: 'Logout', component: 'HomePage'}
+      { title: 'Forum', component: 'ForumPage', backView: true},
+      { title: 'Logout', component: HomePage}
     ];
 
   }
@@ -45,12 +44,16 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
+    if(page.backView){
+      this.nav.push(page.component);
+    } else {
     this.nav.setRoot(page.component, {}, {}, ()=> {
-      Parse.User.logOut().then(()=> {
-        console.log('user logged out');
-      }).catch(()=> {
-        console.log('user logout fail');
+        Parse.User.logOut().then(()=> {
+          console.log('user logged out');
+        }).catch(()=> {
+          console.log('user logout fail');
+        });
       });
-    });
+  }
   }
 }
