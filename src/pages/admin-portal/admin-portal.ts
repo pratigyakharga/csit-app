fca79cbeb51acc72792d195ac3aef5f9bf527c7d
Pratigya Kharga <kchetrip@gmail.com>
2019-07-01 12:40:55 -0500

    updated admin page with dropdowns

--- src/pages/admin-portal/admin-portal.ts
+++ src/pages/admin-portal/admin-portal.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Parse} from 'parse';
 import {AlertController} from 'ionic-angular';
+import {Semester} from "../../models/Semester";
+import {Subject} from "../../models/Subject";
+
 /**
  * Generated class for the AdminPortalPage page.
  *
@@ -16,14 +19,98 @@ import {AlertController} from 'ionic-angular';
 })
 export class AdminPortalPage {
 
-  subject: string = 'Calculus';
-  semester: string = 'First';
-  term: string = 'Mid';
-  year: string = '2019';
+  subject: string;
+  semester: Semester;
+  semesters: Semester[];
+  term: string = 'mid';//mid, unitTest, preBoard, final
+  year: string = '2070';
   pdfChoosen: boolean;
   uploadFile: File;
-
+  Question: any;
   constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtr: AlertController) {
+
+    this.Question = Parse.Object.extend('Questions');
+
+    let firstSem = new Semester("First");
+    firstSem.subjects = [
+      new Subject("Stats"),
+      new Subject("Calculus"),
+      new Subject("FIT"),
+      new Subject("Probability"),
+      new Subject("C Programming "),
+    ];
+    let secondSem = new Semester("Second");
+    secondSem.subjects = [
+      new Subject("Linear Algebra"),
+      new Subject("Data Structure and Algorithms"),
+      new Subject("Discrete Structures"),
+      new Subject("Microprocessor"),
+      new Subject("Digital Logic"),
+    ];
+    let thirdSem = new Semester("Third");
+    thirdSem.subjects = [
+      new Subject("Numerical Methods"),
+      new Subject("Operating Systems"),
+      new Subject("Introduction to Management"),
+      new Subject("Object Oriented Programming"),
+      new Subject("Computer Architecture"),
+    ];
+    let fourthSem = new Semester("Fourth");
+    fourthSem.subjects = [
+      new Subject("Computer Graphics"),
+      new Subject("DBMS"),
+      new Subject("System Analysis and Design"),
+      new Subject("Artificial Intelligence"),
+      new Subject("Theory of Computation"),
+
+    ];
+    let fifthSem = new Semester("Fifth");
+    fifthSem.subjects = [
+      new Subject("Computer Networks"),
+      new Subject("Simulation and Modeling"),
+      new Subject("Design and Analysis of Algorithms"),
+      new Subject("Cryptography"),
+
+
+    ];
+    let sixthSem = new Semester("Sixth");
+    sixthSem.subjects = [
+      new Subject("Software Engineering"),
+      new Subject("Compiler Design and Construction"),
+      new Subject("Web Technologies"),
+      new Subject("Real Time System"),
+      new Subject("Image Processing"),
+    ];
+    let seventhSem = new Semester("Seventh");
+    seventhSem.subjects = [
+      new Subject("Advance DBMS"),
+      new Subject("Internet Technology"),
+      new Subject("Advanced Java Programming"),
+      new Subject("Network and System Administration"),
+    ];
+    let eighthSem = new Semester("Eighth");
+    eighthSem.subjects = [
+      new Subject("Data Warehousing"),
+      new Subject("Internship"),
+      new Subject("Network Security"),
+      new Subject("Cloud Computing"),
+    ];
+
+    this.semesters = [
+      firstSem,
+      secondSem,
+      thirdSem,
+      fourthSem,
+      fifthSem,
+      sixthSem,
+      seventhSem,
+      eighthSem,
+    ];
+
+    // auto populators
+    this.semester = firstSem;
+    this.subject = 'Calculus';
+
   }
 
   ionViewDidLoad() {
@@ -59,7 +146,7 @@ export class AdminPortalPage {
       pdfUrl: url,
       term: this.term,
       subject: this.subject,
-      semester: this.semester,
+      semester: this.semester.name,
       year: parseInt(this.year),
       type: 'pdf'
     };
@@ -84,6 +171,11 @@ export class AdminPortalPage {
       buttons: ['Ok']
     });
     alert.present();
+  }
+
+  onSemesterChange(selectedSemester: Semester) {
+    console.log('changed to : ',selectedSemester);
+    this.subject = undefined;
   }
 
 }
