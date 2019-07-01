d704225d5fb6fc37146c458c9697da29ab5b0ea1
Pratigya Kharga <kchetrip@gmail.com>
2019-07-01 18:04:43 -0500

    able to read uploaded notes

--- src/pages/notes/notes.ts
+++ src/pages/notes/notes.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, LoadingController } from 'ionic-angular';
+import { Parse } from 'parse';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 /**
  * Generated class for the NotesPage page.
@@ -13,13 +15,62 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-notes',
   templateUrl: 'notes.html',
 })
+
 export class NotesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  notes: any[];
+  Note = Parse.Object.extend('Notes');
+  term: any;
+  termMap: any;
+
+  constructor(public navCtrl: NavController, private iab: InAppBrowser, private loadingCtrl: LoadingController) {
+    // first time load
+    this.termMap = {
+      firstTerm: 'First Term',
+      midTerm: 'Mid Term',
+      preBoard: 'Pre Board',
+      final: 'Final (TU)',
+    };
+    this.term = 'firstTerm';
+    this.updateQuestions();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad NotesPage');
+updateQuestions() {
+    // load q according to term
+    console.log(' will load for ', this.term);
+
+    const loader = this.loadingCtrl.create({
+      content: 'Loading ...',
+      duration: 3000
+    });
+    loader.present();
+
+    let query = new Parse.Query(this.Note);
+    let selectedSubject = localStorage.getItem('SELECTED_SUBJECT') || 'Calculus';
+    let selectedSemester = localStorage.getItem('SELECTED_SEMESTER') || 'First';
+
+    // query.equalTo('year', parseInt(this.year));
+    query.equalTo('term', this.term);
+    query.equalTo('subject', selectedSubject);
+    query.equalTo('semester', selectedSemester);
+    query.descending('createdAt');
+
+    query.find().then(response => {
+      console.log('response questions : ', response);
+      this.notes = response.map(q => q.attributes);
+      loader.dismiss();
+    })
+      .catch(err => {
+        console.log(err);
+      });
+
+  }
+
+  showPdf(question) {
+    let pdfUrl = question.pdfUrl;
+    console.log('this is the PDF url : ', pdfUrl);
+    const googleDocLink = 'http://docs.google.com/viewer?url=';
+    this.iab.create(googleDocLink + pdfUrl);
   }
 
 }
--- src/pages/solutions/solutions.ts
+++ src/pages/solutions/solutions.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, LoadingController } from 'ionic-angular';
+import { Parse } from 'parse';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 /**
  * Generated class for the SolutionsPage page.
@@ -15,11 +17,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SolutionsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  solutions: any[];
+  Solution = Parse.Object.extend('Solutions');
+  term: any;
+  termMap: any;
+
+  constructor(public navCtrl: NavController, private iab: InAppBrowser, private loadingCtrl: LoadingController) {
+    // first time load
+    this.termMap = {
+      firstTerm: 'First Term',
+      midTerm: 'Mid Term',
+      preBoard: 'Pre Board',
+      final: 'Final (TU)',
+    };
+    this.term = 'firstTerm';
+    this.updateQuestions();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SolutionsPage');
+updateQuestions() {
+    // load q according to term
+    console.log(' will load for ', this.term);
+
+    const loader = this.loadingCtrl.create({
+      content: 'Loading ...',
+      duration: 3000
+    });
+    loader.present();
+
+    let query = new Parse.Query(this.Solution);
+    let selectedSubject = localStorage.getItem('SELECTED_SUBJECT') || 'Calculus';
+    let selectedSemester = localStorage.getItem('SELECTED_SEMESTER') || 'First';
+
+    // query.equalTo('year', parseInt(this.year));
+    query.equalTo('term', this.term);
+    query.equalTo('subject', selectedSubject);
+    query.equalTo('semester', selectedSemester);
+    query.descending('createdAt');
+
+    query.find().then(response => {
+      console.log('response questions : ', response);
+      this.solutions = response.map(q => q.attributes);
+      loader.dismiss();
+    })
+      .catch(err => {
+        console.log(err);
+      });
+
+  }
+
+  showPdf(question) {
+    let pdfUrl = question.pdfUrl;
+    console.log('this is the PDF url : ', pdfUrl);
+    const googleDocLink = 'http://docs.google.com/viewer?url=';
+    this.iab.create(googleDocLink + pdfUrl);
   }
 
 }
